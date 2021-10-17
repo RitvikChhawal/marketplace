8ce72daf3bd591bed2939acae041810efea7b7a9
RitvikChhawal <ritvikchhawal98349@gmail.com>
2021-10-18 00:00:27 +0530

    fourth module

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,58 +1,6 @@
 import React, { Component } from 'react';
-import Web3 from 'web3';
-import logo from '../logo.png';
-import './App.css';
-import marketplace from '../abis/marketplace.json'
 
 class App extends Component {
-  async componentWillMount(){
-    await this.loadWeb3()
-    // console.log(window.web3)
-    await this.loadBlockchainData()
-  }
- 
-  async loadWeb3(){
-    if(window.ethereum){
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-
-    }
-    else if(window.web3){
-      window.web3 = new Web3(window.web3.currentProvider)     
-    }
-    else{
-      window.alert('Non-Ethereum browser detected. You should install Metamask')
-    }
-  }
-  async loadBlockchainData(){
-    const web3 = window.web3
-    //load account 
-    const accounts = await web3.eth.getAccounts()
-    this.setState({account : accounts[0]})
-    const networkID = await web3.eth.net.getId()
-    const networkData  = marketplace.networks[networkID]
-    if(networkData){
-      const Marketplace = web3.eth.Contract(marketplace.abi, networkData.address)
-      console.log(Marketplace)
-    }else{
-      window.alert('Marketplace contract not deployed to detected network')
-    }
-    // console.log(networkID)
-    // const abi = marketplace.abi
-    // const address = marketplace.networks[networkID].address
-    // const Marketplace = web3.eth.Contract(abi,address)
-    //console.log(accounts) 
-  }
-
-  constructor(props){
-    super(props)
-    this.state = {
-      account: ' ',
-      productCount: 0,
-      products: [],
-      loading: true
-    }
-  }
 
   render() {
     return (
